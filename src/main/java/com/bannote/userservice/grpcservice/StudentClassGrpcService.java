4ee62d85ec6e8ee0b53ccdd0592b5367589d6b56
kyumin1227 <kyumin12271227@gmail.com>
2025-11-07 12:47:25 +0900

    feat: StudentClassGrpcService 메소드 작성

---
+++ src/main/java/com/bannote/userservice/grpcservice/StudentClassGrpcService.java
@@ -0,0 +1,139 @@
+package com.bannote.userservice.grpcservice;
+
+import com.bannote.userservice.context.AuthorizationUtil;
+import com.bannote.userservice.domain.studentclass.StudentClass;
+import com.bannote.userservice.exception.UserServiceException;
+import com.bannote.userservice.proto.student_class.v1.*;
+import com.bannote.userservice.service.studentclass.StudentClassApplicationService;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.grpc.server.service.GrpcService;
+
+import java.util.stream.Collectors;
+
+@GrpcService
+@RequiredArgsConstructor
+public class StudentClassGrpcService extends StudentClassServiceGrpc.StudentClassServiceImplBase {
+
+    private final StudentClassApplicationService service;
+
+    @Override
+    public void getStudentClass(GetStudentClassRequest request, StreamObserver<GetStudentClassResponse> responseObserver) {
+
+        try {
+            StudentClass studentClass = service.getStudentClass(request);
+
+            GetStudentClassResponse response = GetStudentClassResponse.newBuilder()
+                    .setStudentClass(studentClass.toProto())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (UserServiceException e) {
+            responseObserver.onError(
+                    e.getErrorCode().getStatus()
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+        }
+    }
+
+    @Override
+    public void createStudentClass(CreateStudentClassRequest request, StreamObserver<CreateStudentClassResponse> responseObserver) {
+
+        AuthorizationUtil.requireTA();
+
+        try {
+            StudentClass studentClass = service.createStudentClass(request);
+
+            CreateStudentClassResponse response = CreateStudentClassResponse.newBuilder()
+                    .setStudentClass(studentClass.toProto())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (UserServiceException e) {
+            responseObserver.onError(
+                    e.getErrorCode().getStatus()
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+        }
+    }
+
+    @Override
+    public void updateStudentClass(UpdateStudentClassRequest request, StreamObserver<UpdateStudentClassResponse> responseObserver) {
+
+        AuthorizationUtil.requireTA();
+
+        try {
+            StudentClass studentClass = service.updateStudentClass(request);
+
+            UpdateStudentClassResponse response = UpdateStudentClassResponse.newBuilder()
+                    .setStudentClass(studentClass.toProto())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (UserServiceException e) {
+            responseObserver.onError(
+                    e.getErrorCode().getStatus()
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+        }
+    }
+
+    @Override
+    public void deleteStudentClass(DeleteStudentClassRequest request, StreamObserver<DeleteStudentClassResponse> responseObserver) {
+
+        AuthorizationUtil.requireTA();
+
+        try {
+            StudentClass studentClass = service.deleteStudentClass(request);
+
+            DeleteStudentClassResponse response = DeleteStudentClassResponse.newBuilder()
+                    .setStudentClass(studentClass.toProto())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (UserServiceException e) {
+            responseObserver.onError(
+                    e.getErrorCode().getStatus()
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+        }
+    }
+
+    @Override
+    public void listStudentClasses(ListStudentClassesRequest request, StreamObserver<ListStudentClassesResponse> responseObserver) {
+
+        try {
+            Page<StudentClass> studentClassPage = service.listStudentClasses(request);
+
+            ListStudentClassesResponse response = ListStudentClassesResponse.newBuilder()
+                    .addAllStudentClasses(studentClassPage.getContent().stream()
+                            .map(StudentClass::toProto)
+                            .collect(Collectors.toList()))
+                    .setTotalCount(Math.toIntExact(studentClassPage.getTotalElements()))
+                    .setPage(request.getPage())
+                    .setSize(request.getSize())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (UserServiceException e) {
+            responseObserver.onError(
+                    e.getErrorCode().getStatus()
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+        }
+    }
+}
---
+++ src/main/java/com/bannote/userservice/service/studentclass/StudentClassApplicationService.java
@@ -0,0 +1,29 @@
+package com.bannote.userservice.service.studentclass;
+
+import com.bannote.userservice.domain.studentclass.StudentClass;
+import com.bannote.userservice.proto.student_class.v1.*;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentClassApplicationService {
+    public StudentClass getStudentClass(GetStudentClassRequest request) {
+        return null;
+    }
+
+    public StudentClass createStudentClass(CreateStudentClassRequest request) {
+        return null;
+    }
+
+    public StudentClass updateStudentClass(UpdateStudentClassRequest request) {
+        return null;
+    }
+
+    public StudentClass deleteStudentClass(DeleteStudentClassRequest request) {
+        return null;
+    }
+
+    public Page<StudentClass> listStudentClasses(ListStudentClassesRequest request) {
+        return null;
+    }
+}
