b7a74cae3c507800d1d5ea7e2c3d600d1ee72b91
kyumin1227 <kyumin12271227@gmail.com>
2025-11-11 20:47:18 +0900

    feat: gRPC exception handler 작성
    - gRPC의 인터셉터로 요청 중에 에러 발생 시 처리하는 핸들러 작성

--- src/main/java/com/bannote/userservice/config/GrpcConfig.java
+++ src/main/java/com/bannote/userservice/config/GrpcConfig.java
@@ -1,5 +1,6 @@
 package com.bannote.userservice.config;
 
+import com.bannote.userservice.exception.GrpcExceptionHandler;
 import com.bannote.userservice.interceptor.GrpcAuthInterceptor;
 import io.grpc.ServerInterceptor;
 import lombok.RequiredArgsConstructor;
@@ -11,13 +12,28 @@ import org.springframework.grpc.server.GlobalServerInterceptor;
 
 /**
  * gRPC 서버 설정
- * 인증 인터셉터를 gRPC 서버에 등록
+ * 전역 예외 핸들러 및 인증 인터셉터를 gRPC 서버에 등록
  */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class GrpcConfig {
 
+    /**
+     * 전역 예외 핸들러
+     * Order 50으로 가장 먼저 실행되어 모든 예외를 캐치
+     */
+    @Bean
+    @Order(50)
+    @GlobalServerInterceptor
+    ServerInterceptor grpcExceptionHandler() {
+        return new GrpcExceptionHandler();
+    }
+
+    /**
+     * 인증 인터셉터
+     * Order 100으로 예외 핸들러 다음에 실행
+     */
     @Bean
     @Order(100)
     @GlobalServerInterceptor
