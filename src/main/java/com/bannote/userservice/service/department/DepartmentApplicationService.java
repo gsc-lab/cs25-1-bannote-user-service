ef540fd2890cbb2a66037bc40fa425692c856f3f
kyumin1227 <kyumin12271227@gmail.com>
2025-11-18 16:41:31 +0900

    feat: 학과 이벤트 작성
    - 트랜잭션 커밋 후 kafka로 발행 하도록 리스너 작성
    - proto의 형식으로 변환하는 빌더 작성
    - ApplicationService에서 CommandService 종료 이후 이벤트 발행

--- src/main/java/com/bannote/userservice/service/department/DepartmentApplicationService.java
+++ src/main/java/com/bannote/userservice/service/department/DepartmentApplicationService.java
@@ -1,18 +1,22 @@
 package com.bannote.userservice.service.department;
 
+import com.bannote.userservice.context.AuthorizationUtil;
 import com.bannote.userservice.domain.department.Department;
 import com.bannote.userservice.domain.department.field.DepartmentCode;
 import com.bannote.userservice.domain.department.field.DepartmentName;
 import com.bannote.userservice.entity.DepartmentEntity;
+import com.bannote.userservice.event.department.DepartmentCreatedEvent;
+import com.bannote.userservice.event.department.DepartmentDeletedEvent;
+import com.bannote.userservice.event.department.DepartmentUpdatedEvent;
 import com.bannote.userservice.proto.department.v1.*;
 import com.google.protobuf.ProtocolStringList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -21,6 +25,7 @@ public class DepartmentApplicationService {
 
     private final DepartmentCommandService departmentCommandService;
     private final DepartmentQueryService departmentQueryService;
+    private final ApplicationEventPublisher eventPublisher;
 
     /**
      * 학과 생성
@@ -34,7 +39,16 @@ public class DepartmentApplicationService {
                 DepartmentName.of(request.getDepartmentName())
         );
 
-        return departmentCommandService.createDepartment(department);
+        Department createdDepartment = departmentCommandService.createDepartment(department);
+
+        eventPublisher.publishEvent(
+                new DepartmentCreatedEvent(
+                        createdDepartment,
+                        AuthorizationUtil.getCurrentAuthInfo().userCode().getValue()
+                )
+        );
+
+        return createdDepartment;
     }
 
     /**
@@ -59,7 +73,16 @@ public class DepartmentApplicationService {
                 request.hasName() ? DepartmentName.of(request.getName()) : null
         );
 
-        return departmentCommandService.updateDepartment(department);
+        Department updatedDepartment = departmentCommandService.updateDepartment(department);
+
+        eventPublisher.publishEvent(
+                new DepartmentUpdatedEvent(
+                        updatedDepartment,
+                        AuthorizationUtil.getCurrentAuthInfo().userCode().getValue()
+                )
+        );
+
+        return updatedDepartment;
     }
 
     /**
@@ -69,7 +92,16 @@ public class DepartmentApplicationService {
      */
     public Department deleteDepartment(DeleteDepartmentRequest request) {
 
-        return departmentCommandService.deleteDepartment(DepartmentCode.of(request.getDepartmentCode()));
+        Department deletedDepartment = departmentCommandService.deleteDepartment(DepartmentCode.of(request.getDepartmentCode()));
+
+        eventPublisher.publishEvent(
+                new DepartmentDeletedEvent(
+                        deletedDepartment,
+                        AuthorizationUtil.getCurrentAuthInfo().userCode().getValue()
+                )
+        );
+
+        return deletedDepartment;
     }
 
     /**
