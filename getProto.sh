ece17e41e484d2e86d1202ccdd09b8a50a412dcc
kyumin1227 <kyumin12271227@gmail.com>
2025-11-18 15:49:37 +0900

    chore: proto로 정의된 kafka 파일 형식 import 설정
    - common-service의 enums 및 user_events 가져옴

--- getProto.sh
+++ getProto.sh
@@ -1,2 +1,3 @@
 rm -rf /Volumes/Repository/user-service/src/main/proto
 buf export buf.build/bannote/user-service -o /Volumes/Repository/user-service/src/main/proto
+buf export buf.build/bannote/common-service -o /Volumes/Repository/user-service/src/main/proto
