b99e6bf52b699572d58ac9a2a02ad27043f1847e
kyumin1227 <kyumin12271227@gmail.com>
2025-11-18 15:51:07 +0900

    feat: event 형식으로 구현함에 따른 이벤트 클래스 정의
    - transaction을 고려하여 kafka 발행은 이벤트 형식으로 구현

---
+++ src/main/java/com/bannote/userservice/event/studentclass/StudentClassDeletedEvent.java
@@ -0,0 +1,16 @@
+package com.bannote.userservice.event.studentclass;
+
+import com.bannote.userservice.domain.studentclass.StudentClass;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 학반 삭제 도메인 이벤트
+ * ApplicationService에서 발행하고, EventListener에서 Kafka로 전송
+ */
+@Getter
+@RequiredArgsConstructor
+public class StudentClassDeletedEvent {
+    private final StudentClass studentClass;
+    private final String triggeredBy;
+}
