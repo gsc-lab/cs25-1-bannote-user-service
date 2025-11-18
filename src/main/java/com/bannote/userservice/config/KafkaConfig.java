5475b180913870894cacc0a71f8db65c2b0bfeae
kyumin1227 <kyumin12271227@gmail.com>
2025-11-18 10:24:54 +0900

    feat: 카프카 로컬 환경 배포 및 연결 설정
    - docker-compose를 이용하여 kafka 및 kafka-ui 배포
    - kafka 라이브러리 설치
    - kafka 연결 설정 및 topic 작성

---
+++ src/main/java/com/bannote/userservice/config/KafkaConfig.java
@@ -0,0 +1,33 @@
+package com.bannote.userservice.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9095");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("user.changed")
+                        .build(),
+                TopicBuilder.name("department.changed")
+                        .build(),
+                TopicBuilder.name("student-class.changed")
+                        .build()
+        );
+    }
+}
