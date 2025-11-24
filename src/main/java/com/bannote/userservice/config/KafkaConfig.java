714b8e12a11d9a9cdd908092059b052ded36d7b6
Kyumin Kim <kyumin12271227@gmail.com>
2025-11-24 17:47:18 +0900

    fix: test시 kafka 의존성 제거 프로파일 기반으로 분리 (#24)
    - test 시 "test" 프로파일 이용하여 KafkaTemplate는 KafkaConfig가 아닌 TestConfig 의존

--- src/main/java/com/bannote/userservice/config/KafkaConfig.java
+++ src/main/java/com/bannote/userservice/config/KafkaConfig.java
@@ -5,9 +5,9 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
@@ -17,7 +17,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-@ConditionalOnProperty(name = "spring.kafka.bootstrap-servers")
+@Profile("!test")
 @Configuration
 public class KafkaConfig {
 
--- src/test/java/com/bannote/userservice/config/TestConfig.java
+++ src/test/java/com/bannote/userservice/config/TestConfig.java
@@ -1,14 +1,19 @@
 package com.bannote.userservice.config;
 
-import com.bannote.userservice.event.EventPublisher;
 import com.google.protobuf.Message;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import static org.mockito.Mockito.mock;
+
+@Profile("test")
 @Configuration
 public class TestConfig {
 
-    @MockitoBean(types = {EventPublisher.class})
-    private KafkaTemplate<String, Message> kafkaTemplate;
+    @Bean
+    public KafkaTemplate<String, Message> kafkaTemplate() {
+        return mock(KafkaTemplate.class);
+    }
 }
